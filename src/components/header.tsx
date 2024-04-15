cfeb0a45e5c970abdd6a719446b44c39cb6ccadd
justartiom <artiomsiatat@gmail.com>
2024-04-15 21:44:50 +0100

    init

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header() {
-    return <div className="w-full h-[72px] bg-background"></div>;
+    return <div className="w-full h-[72px] bg-background">lol</div>;
 }
 
 export default Header;

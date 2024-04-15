dbbd1557771906ea556aef7b49a9f54640b593de
justartiom <artiomsiatat@gmail.com>
2024-04-15 22:36:23 +0100

    router

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Header() {
-    return <div className="w-full h-[72px] bg-background">lol</div>;
+    const [currentSection, setCurrentSection] = useState("");
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const sections = document.querySelectorAll("section");
+            let current = "";
+
+            sections.forEach((section) => {
+                const sectionTop = section.offsetTop;
+                const sectionHeight = section.clientHeight;
+                if (
+                    window.scrollY >= sectionTop &&
+                    window.scrollY < sectionTop + sectionHeight
+                ) {
+                    current = section.id;
+                }
+            });
+
+            console.log(current);
+            setCurrentSection(current);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <div className="fixed w-full h-[72px] bg-background flex">
+            <div className="flex-1 grid items-center">
+                <p
+                    style={{
+                        textShadow: "0 0 10px #FF9F9F",
+                    }}
+                    className="text-center text-2xl font-semibold shadow-neon"
+                >
+                    Artiom
+                </p>
+            </div>
+            <div className="flex-1 flex items-center justify-center">
+                {[
+                    { t: "Home", h: "#home" },
+                    { t: "About Me", h: "#aboutme" },
+                    { t: "Projects", h: "#projects" },
+                    { t: "Get In Touch", h: "#contactme" },
+                ].map((l) => (
+                    <a
+                        key={l.h}
+                        href={l.h}
+                        className={
+                            "px-2 py-1.5 mx-2 text-sm rounded-lg bg-primary " +
+                            (false ? "bg-primary text-black" : "") // True if the user is in the right section
+                        }
+                    >
+                        {l.t}
+                    </a>
+                ))}
+            </div>
+            <div className="flex-1">Contact me</div>
+        </div>
+    );
 }
 
 export default Header;
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Header from "testtt/header";
+import Header from "components/header";
 
 function App() {
     return (
         <div>
             <Header />
+            <div id="home" className="w-full h-[100vh]"></div>
+            <div id="aboutme" className="w-full h-[100vh]"></div>
+            <div id="projects" className="w-full h-[100vh]"></div>
+            <div id="contactme" className="w-full h-[100vh]"></div>
         </div>
     );
 }

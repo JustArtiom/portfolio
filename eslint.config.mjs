d93e20d558a52821b5008624093614a7ec695107
JustArtiom <artiomsiatat@gmail.com>
2026-04-20 17:35:53 +0100

    Fix eslint (again)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -45,10 +45,18 @@ export default defineConfig([
       `frontend/**/*.ts`,
       `frontend/**/*.tsx`,
     ],
-    extends: [
-      reactHooks.configs.flat[`recommended-latest`],
-      reactRefresh.configs.vite,
-    ],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": `error`,
+      "react-hooks/exhaustive-deps": `warn`,
+      "react-refresh/only-export-components": [
+        `warn`,
+        { allowConstantExport: true },
+      ],
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
